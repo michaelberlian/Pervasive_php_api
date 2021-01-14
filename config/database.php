e963b2874df0fa29b1a43a1033a59639df614a70
Michael Berlian <michaelberlian01@gmail.com>
2021-01-14 13:08:39 +0700

    -addition

--- config/database.php
+++ config/database.php
@@ -2,7 +2,7 @@
     class Database {
         // params
         private $host = 'localhost';
-        private $db_name = 'myblog';
+        private $db_name = 'testing_api';
         private $username = 'root';
         private $password = '';
         private $conn;
@@ -12,10 +12,14 @@
             $this->conn = null; 
 
             try{
-
+                $this->conn = new PDO('mysql:host=' . $this->host . ';dbname=' . $this->db_name, 
+                $this->username, $this->password);
+                $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             } catch (PDOException $e){
                 echo 'Connection Error: ' . $e->getMessage();
             }
+
+            return $this->conn;
         }
     }
-?>
+?>z
